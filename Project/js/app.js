bfed31f0b2e0c2526e931f55b0bed1d8912e26f5
Sergey <anfalov@camini-pr.ru>
2020-12-07 17:36:24 +0500

    подготовили к написанию скрипта по переключение разделов

--- Project/js/app.js
+++ Project/js/app.js
@@ -14,7 +14,6 @@ function email_test(input) {
 // let playFirstVideo = document.querySelector('#play1');
 // console.log(playFirstVideo)
 
-
 var ua = window.navigator.userAgent;
 var msie = ua.indexOf("MSIE ");
 var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
@@ -331,7 +330,7 @@ function popup_open(item, video = '') {
 	if (curent_popup && unlock) {
 		if (video != '' && video != null) {
 			let popup_video = document.querySelector('.popup_video');
-			popup_video.querySelector('.popup__video').innerHTML = '<iframe src="https://www.youtube.com/embed/'+ video +'?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+			popup_video.querySelector('.popup__video').innerHTML = '<iframe src="https://www.youtube.com/embed/'+ video +'?autoplay=1&modestbranding=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 		}																								
 		if (!document.querySelector('.menu__body._active')) {
 			body_lock_add(800);
@@ -1395,7 +1394,9 @@ if (sliders) {
 
 function sliders_bild_callback(params) {}
 
-let slider_about = new Swiper('.swiper-container', {
+
+//Слайдер для Блок вентилятора
+let slider_block = new Swiper('.swiper-container__block', {
 	/*
 	effect: 'fade',
 	*/
@@ -1417,7 +1418,196 @@ let slider_about = new Swiper('.swiper-container', {
 	lazy: true,
 	// Dotts
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.swiper-pagination__block',
+		clickable: true,
+	},
+	// Arrows
+	// navigation: {
+	// 	nextEl: '.about__more .more__item_next',
+	// 	prevEl: '.about__more .more__item_prev',
+	// },
+	/*
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: true,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		1268: {
+			slidesPerView: 4,
+			spaceBetween: 30,
+		},
+	},
+	*/
+	on: {
+		lazyImageReady: function () {
+			ibg();
+		},
+	}
+	// And if we need scrollbar
+	// scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// }
+});
+
+// Слайдер для Регулирующие клапаны
+let slider_valve = new Swiper('.swiper-container__valve', {
+	/*
+	effect: 'fade',
+	*/
+	// autoplay: {
+	// 	delay: 5000,
+	// 	disableOnInteraction: false,
+	// },
+	observer: true,
+	observeParents: true,
+	slidesPerView: 1,
+	spaceBetween: 0,
+	autoHeight: true,
+	speed: 800,
+	//touchRatio: 0,
+	//simulateTouch: false,
+	loop: true,
+	draggable: true,
+	//preloadImages: false,
+	lazy: true,
+	// Dotts
+	pagination: {
+		el: '.swiper-pagination__valve',
+		clickable: true,
+	},
+	// Arrows
+	// navigation: {
+	// 	nextEl: '.about__more .more__item_next',
+	// 	prevEl: '.about__more .more__item_prev',
+	// },
+	/*
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: true,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		1268: {
+			slidesPerView: 4,
+			spaceBetween: 30,
+		},
+	},
+	*/
+	on: {
+		lazyImageReady: function () {
+			ibg();
+		},
+	}
+	// And if we need scrollbar
+	// scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// }
+});
+
+// Слайдер для Регулирующие клапаны
+let slider_collector = new Swiper('.swiper-container__collector', {
+	/*
+	effect: 'fade',
+	*/
+	// autoplay: {
+	// 	delay: 5000,
+	// 	disableOnInteraction: false,
+	// },
+	observer: true,
+	observeParents: true,
+	slidesPerView: 1,
+	spaceBetween: 0,
+	autoHeight: true,
+	speed: 800,
+	//touchRatio: 0,
+	//simulateTouch: false,
+	loop: true,
+	draggable: true,
+	//preloadImages: false,
+	lazy: true,
+	// Dotts
+	pagination: {
+		el: '.swiper-pagination__collector',
+		clickable: true,
+	},
+	// Arrows
+	// navigation: {
+	// 	nextEl: '.about__more .more__item_next',
+	// 	prevEl: '.about__more .more__item_prev',
+	// },
+	/*
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: true,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		1268: {
+			slidesPerView: 4,
+			spaceBetween: 30,
+		},
+	},
+	*/
+	on: {
+		lazyImageReady: function () {
+			ibg();
+		},
+	}
+	// And if we need scrollbar
+	// scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// }
+});
+
+// Слайдер для Приложение для Healthbox
+let slider_app = new Swiper('.swiper-container__app', {
+	/*
+	effect: 'fade',
+	*/
+	// autoplay: {
+	// 	delay: 5000,
+	// 	disableOnInteraction: false,
+	// },
+	observer: true,
+	observeParents: true,
+	slidesPerView: 1,
+	spaceBetween: 0,
+	autoHeight: true,
+	speed: 800,
+	//touchRatio: 0,
+	//simulateTouch: false,
+	loop: true,
+	draggable: true,
+	//preloadImages: false,
+	lazy: true,
+	// Dotts
+	pagination: {
+		el: '.swiper-pagination__app',
 		clickable: true,
 	},
 	// Arrows
